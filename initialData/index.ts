11f347e393454a6edafe5af6e930f78b292ba915
awwwdev <->
2024-09-14 11:09:26 +0330

    feat: init helper

---
+++ initialData/index.ts
@@ -0,0 +1,98 @@
+import { Folder, List, Settings, Task } from "@/types";
+import { v4 as uuid } from "uuid";
+
+export const makeEmptyTask = (): Task => ({
+  id: uuid(),
+  label: "",
+  dateCreated: new Date(),
+  status: "todo",
+  lists: {
+    all: {
+      dateAddedToList: new Date(),
+      orderInList: -1,
+    },
+  },
+});
+
+export const createInitialSettings = (): Settings => ({ 
+  theme: "system",
+  showCompletedTasks: true,
+  startOfWeek: "saturday",
+  planForTommorowSettings: {},
+});
+
+
+export const createInitialFolders = (): Folder[] => ([
+  {
+    id: "1",
+    name: "Folder 1",
+    dateCreated: new Date(),
+    emojies: ["📬"],
+  },
+  {
+    id: "2",
+    name: "Folder 2",
+    dateCreated: new Date(),
+    emojies: ["📅"],
+  },
+]);
+
+export const createInitialLists = (): List[] => (
+  [
+    {
+      id: "1",
+      name: "All Tasks",
+      dateCreated: new Date(),
+      emojies: ["📝"],
+      description: "This is a list 1",
+      filter: (t: Task) => true,
+      show: true,
+      orderInFolder: 0,
+      folders: null,
+    },
+    {
+      id: "2",
+      name: "Do Today",
+      dateCreated: new Date(),
+      emojies: ["📝"],
+      description: "This is a list 2",
+      filter: (t: Task) => true,
+      show: true,
+      orderInFolder: 0,
+      folders: null,
+    },
+    {
+      id: "3",
+      name: "Do Tomorrow",
+      dateCreated: new Date(),
+      emojies: ["📝"],
+      description: "This is a list 3",
+      filter: (t: Task) => true,
+      show: true,
+      orderInFolder: 0,
+      folders: null,
+    },
+    {
+      id: "4",
+      name: "Starred",
+      dateCreated: new Date(),
+      emojies: ["📝"],
+      description: "This is a list 4",
+      filter: (t: Task) => true,
+      show: true,
+      orderInFolder: 0,
+      folders: null,
+    },  
+    {
+      id: "5",
+      name: "With Due Dates",
+      dateCreated: new Date(),
+      emojies: ["📝"],
+      description: "This is a list 5",
+      filter: (t: Task) => true,
+      show: true,
+      orderInFolder: 0,
+      folders: null,
+    },
+  ]
+)
