db74aeddbc7d4b6790c37184e03394a2e8a9120d
awwwdev <->
2024-09-24 16:51:15 +0330

    feat: connect settings to api

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -2,12 +2,33 @@
 
 import { useGlobalContex } from "@/components/Provider";
 import Icon from '@/components/ui/Icon';
+import LoadingSpinner from '@/components/ui/LoadingSpinner';
 import RadioGroup from "@/components/ui/RadioGroup";
+import useUserMe from '@/hooks/userMe';
+import QUERY_KEYS from '@/react-query/queryKeys';
 import { Settings } from "@/types";
+import fetchAPI from '@/utils/fetchAPI';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import Link from 'next/link';
+import toast from 'react-hot-toast';
 
 export default function Page() {
-  const { settings, setSettings } = useGlobalContex();
+  const userMeQ = useUserMe();
+  const settings = userMeQ.data?.settings;
+  const queryClient = useQueryClient();
+  const settingsMutation = useMutation({
+    mutationFn: async (changedSettings: Partial<Settings>) => fetchAPI.PUT(`/settings/${userMeQ.data?._id}`, changedSettings),
+    onError: (err) => {
+      toast.error("Something went wrong: " + err.message);
+    },
+    onSuccess: (data) => {
+      toast.success("Settings updated successfully");
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.USERME] });
+    },
+  });
+
+  if (userMeQ.isLoading || !settings) return <LoadingSpinner />;
+
   return (
     <div className="p-6 max-w-page mx-auto">
       <Link href="/" className="c-base11 text-sm">
@@ -20,7 +41,7 @@ export default function Page() {
         legend={<span className="H3">Theme</span>}
         name="theme"
         value={settings.theme}
-        setValue={(th) => setSettings({ ...settings, theme: th as Settings["theme"] })}
+        setValue={(th) => settingsMutation.mutate({ theme: th as Settings["theme"] })}
       >
         <RadioGroup.Item label="Light" value="light"></RadioGroup.Item>
         <RadioGroup.Item label="Dark" value="dark"></RadioGroup.Item>
@@ -31,7 +52,7 @@ export default function Page() {
         legend={<span className="H3">Start of Week</span>}
         name="startOfWeek"
         value={settings.startOfWeek}
-        setValue={(s) => setSettings({ ...settings, startOfWeek: s as Settings["startOfWeek"] })}
+        setValue={(s) => settingsMutation.mutate({ startOfWeek: s as Settings["startOfWeek"] })}
       >
         <RadioGroup.Item label="Sunday" value="sunday"></RadioGroup.Item>
         <RadioGroup.Item label="Monday" value="monday"></RadioGroup.Item>
