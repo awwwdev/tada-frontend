4a683ac86f52dbd57fdf46585714ee9c3f5aa130
awwwdev <->
2024-09-24 08:51:11 +0330

    fix: use fetchAPI instead of fetch

--- hooks/userMe.ts
+++ hooks/userMe.ts
@@ -1,18 +1,15 @@
-import { API } from '@/consts';
-import { User } from '@/types';
-import { useQuery } from '@tanstack/react-query';
+import { API } from "@/consts";
+import { User } from "@/types";
+import fetchAPI from "@/utils/fetchAPI";
+import { useQuery } from "@tanstack/react-query";
 
-export default function useUserMe(){
+export default function useUserMe() {
   return useQuery<User, Error>({
     queryKey: ["userMe"],
     queryFn: async () => {
-      const res = await fetch(`${API}/auth/user`, {
-        credentials: "include",
-        headers: {
-          
-        }
-      });
+      const res = await fetchAPI.GET(`/auth/user`);
       const json = await res.json();
       return json.user;
     },
-  })}
+  });
+}
