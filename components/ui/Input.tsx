d24dcd31f9ba85caf37833b6b64589c27e973cfc
awwwdev <->
2024-10-04 11:04:38 +0330

    fix: ui kit

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,45 +1,64 @@
 import { HTMLProps } from "@/types";
-import { forwardRef, useId } from "react";
+import { CSSProperties, forwardRef, useId } from "react";
 import Label from "./Label";
-import ErrorMessage from "./ErrorMessage";
-
-const classes = {
-  hint: "text-xs c-base11 italic pt-0.3em pb-0.5em",
-  wrapper: {
-    base: `h-2.75em  rd-0.5em leading-1em overflow-hidden  whitespace-nowrap
-     b-1 b-base7 hover:b-base8
-     bg-base3 
-     grid grid-auto-flow-col
-     focus-within:outline-transparent focus-within:b-accent9 focus-within:hover:b-accent9 `,
-    disabled: "aria-[disabled]:cursor-not-allowed  aria-[disabled]:c-base10",
-  },
-  prefixBox: "",
-  inputElement: {
-    base: ` px-0.75em  py-0.375em bg-transparent focus:outline-transparent line-height-1`,
-    disabled: "",
-  },
-  suffixBox: "flex",
-  errorMessage: "",
-};
+import { getSizeStyles, Size, ui } from "./ui-config";
 
 type InputProps = {
-  name: string;
+  name?: string;
   label?: React.ReactNode;
   hint?: string;
   required?: boolean;
-  prefix?: React.ReactNode;
+  prefixx?: React.ReactNode;
   suffix?: React.ReactNode;
+  outerPrefix?: React.ReactNode;
+  outerSuffix?: React.ReactNode;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   setValue?: (val: string) => void;
   errorMessage?: string;
+  uiSize?: Size;
 } & HTMLProps<"input">;
 type Ref = HTMLInputElement;
 type AllProps = React.ComponentPropsWithoutRef<"input"> & InputProps;
 
 const Input = forwardRef<Ref, AllProps>(function (
-  { hint, label, name, className, required, prefix, suffix, disabled, onChange, setValue, errorMessage, ...props },
+  {
+    hint,
+    label,
+    name,
+    className,
+    required,
+    prefixx,
+    suffix,
+    outerSuffix,
+    outerPrefix,
+    disabled,
+    onChange,
+    setValue,
+    errorMessage,
+    uiSize = "md",
+    ...props
+  },
   ref
 ) {
+  const classes = {
+    hint: "text-xs c-base11 italic pt-0.3em pb-0.5em",
+    wrapper: {
+      base: `${ui.h[uiSize]} ${ui.rd[uiSize]} leading-1em overflow-hidden  whitespace-nowrap
+       b-1 b-base7 hover:b-base8
+       bg-base3 
+       grid grid-auto-flow-col items-center
+       focus-within:outline-transparent focus-within:b-accent9 focus-within:hover:b-accent9 `,
+      disabled: "aria-[disabled]:cursor-not-allowed  aria-[disabled]:c-base10",
+    },
+    prefixBox: "",
+    inputElement: {
+      base: ` ${ui.pd.x.xs} ${ui.h[uiSize]} bg-transparent focus:outline-none focus:outline-transpanrent line-height-1`,
+      disabled: "",
+    },
+    suffixBox: "",
+    errorMessage: "",
+  };
+
   const id = useId();
 
   const hintId = hint ? `${id}-hint` : "";
@@ -53,11 +72,19 @@ const Input = forwardRef<Ref, AllProps>(function (
       <Hint id={hintId} hint={hint} />
       <div
         className={`${classes.wrapper.base} ${disabled && classes.wrapper.disabled} `}
-        style={{
-          gridTemplateColumns: `${prefix ? "auto" : ""} 1fr ${suffix ? "auto" : ""}`,
-        }}
+        style={
+          {
+            gridTemplateColumns: `${outerPrefix ? "auto" : ""} ${prefixx ? "auto" : ""} 1fr ${suffix ? "auto" : ""} ${
+              outerSuffix ? "auto" : ""
+            }`,
+            ...getSizeStyles(uiSize),
+            padding: undefined,
+            "--padding": getSizeStyles(uiSize).padding,
+          } as CSSProperties
+        }
       >
-        <PrefixBox prefix={prefix} />
+        <OuterPrefixBox outerPrefix={prefixx} />
+        <PrefixBox prefix={prefixx}  />
         <input
           ref={ref}
           name={name}
@@ -69,9 +96,13 @@ const Input = forwardRef<Ref, AllProps>(function (
             setValue?.(e.target.value);
             onChange?.(e);
           }}
+          style={{
+            padding: getSizeStyles(uiSize).padding,
+          }}
           {...props}
         />
-        <SuffixBox suffix={suffix} />
+        <SuffixBox suffix={suffix}  />
+        <OuterSuffixBox outerSuffix={outerSuffix} />
       </div>
       {/* <div className="line-clamp-1" style={{height: 'var(--line-height)'}}>
         <ErrorMessage id={errorMessageId} >{errorMessage}</ErrorMessage>
@@ -92,13 +123,58 @@ function Hint({ hint, id }) {
   );
 }
 
-function PrefixBox({ prefix }) {
+function PrefixBox({ prefix }: { prefix?: React.ReactNode;}) {
   if (!prefix) return <></>;
-
-  return <div className={classes.prefixBox}>{prefix}</div>;
+  return (
+    <div
+      className={`h-full flex items-center `}
+      style={{
+        paddingInline: "var(--padding)",
+      }}
+    >
+      {prefix}
+    </div>
+  );
 }
 
-function SuffixBox({ suffix }) {
+function SuffixBox({ suffix }: { suffix?: React.ReactNode;  }) {
   if (!suffix) return <></>;
-  return <div className={classes.suffixBox}>{suffix}</div>;
+  return (
+    <div
+      className={`h-full flex items-center`}
+      style={{
+        paddingInline: "var(--padding)",
+      }}
+    >
+      {suffix}
+    </div>
+  );
+}
+
+function OuterPrefixBox({ outerPrefix }: { outerPrefix?: React.ReactNode; }) {
+  if (!outerPrefix) return <></>;
+  return (
+    <div
+      className={`bg-base6 h-full flex items-center`}
+      style={{
+        paddingInline: "var(--padding)",
+      }}
+    >
+      {outerPrefix}
+    </div>
+  );
+}
+
+function OuterSuffixBox({ outerSuffix }: { outerSuffix?: React.ReactNode;  }) {
+  if (!outerSuffix) return <></>;
+  return (
+    <div
+      className={`bg-base6 h-full  flex items-center`}
+      style={{
+        paddingInline: "var(--padding)",
+      }}
+    >
+      {outerSuffix}
+    </div>
+  );
 }
